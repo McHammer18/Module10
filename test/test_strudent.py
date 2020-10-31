907ced7d616953742274aa31f4a77eed4c6173b0
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-31 10:28:02 -0500

    Start of unit testing first test passes

--- test/test_strudent.py
+++ test/test_strudent.py
@@ -1,9 +1,20 @@
 import unittest
-
+from class_definitions import Student as s
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.student = s.Student('Doe', 'John', 'Space exploration')
+
+    def tearDown(self):
+        del self.student
+
+    def test_object_created_required_attributes(self):
+        self.assertEqual(self.student.first_name, "John")
+        self.assertEqual(self.student.last_name, "Doe")
+        self.assertEqual(self.student.major, "Space exploration")
+
+
+
 
 
 if __name__ == '__main__':
