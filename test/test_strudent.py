64f7ad3ba25af71c88acb3be2f858d48276d85bb
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-31 10:35:50 -0500

    Unit test for checking the str method passes

--- test/test_strudent.py
+++ test/test_strudent.py
@@ -3,21 +3,27 @@ from class_definitions import Student as s
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.student = s.Student('Doe', 'John', 'Space Exploration', 3.7)
+        self.student1 = s.Student('Doe', 'John', 'Space Exploration')
+        self.student2 = s.Student('Doe', 'John', 'Space Exploration', 3.7)
 
     def tearDown(self):
-        del self.student
+        del self.student1
+        del self.student2
 
     def test_object_created_required_attributes(self):
-        self.assertEqual(self.student.first_name, "John")
-        self.assertEqual(self.student.last_name, "Doe")
-        self.assertEqual(self.student.major, "Space Exploration")
+        self.assertEqual(self.student1.first_name, "John")
+        self.assertEqual(self.student1.last_name, "Doe")
+        self.assertEqual(self.student1.major, "Space Exploration")
+        self.assertEqual(self.student1.gpa, 0.0)
 
     def test_object_created_all_attributes(self):
-        self.assertEqual(self.student.first_name, "John")
-        self.assertEqual(self.student.last_name, "Doe")
-        self.assertEqual(self.student.major, "Space Exploration")
-        self.assertEqual(self.student.gpa, 3.7)
+        self.assertEqual(self.student2.first_name, "John")
+        self.assertEqual(self.student2.last_name, "Doe")
+        self.assertEqual(self.student2.major, "Space Exploration")
+        self.assertEqual(self.student2.gpa, 3.7)
+
+    def test_student_str(self):
+        self.assertEqual(str(self.student2), "Doe, John has major Space Exploration with gpa: 3.7")
 
 
 
