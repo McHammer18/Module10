df64debde395e92b9d30f5f20c23be64f4e5d3a2
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-31 10:51:41 -0500

    Unit test for checking the major and gpa attributes passes

--- class_definitions/Student.py
+++ class_definitions/Student.py
@@ -9,15 +9,21 @@ Module 10: unit testing classes
 
 class Student:
     """Student class"""
+    MAJORS = ('Liberal Arts', 'Comp Science', 'Math', 'English', 'Criminal Justice', 'Cybersecurity')
     def __init__(self, lname, fname, major, gpa=0.0):
         name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
 
         if not (name_characters.issuperset(lname) and name_characters.issuperset(fname)):
             raise ValueError
+        if major not in self.MAJORS:
+            raise ValueError
+        if not 0.0 <= gpa <= 4.0:
+            raise ValueError
         self.last_name = lname
         self.first_name = fname
         self.major = major
         self.gpa = gpa
+
 
     def __str__(self):
         return self.last_name + ", " + self.first_name + " has major " + self.major + " with gpa: " + str(self.gpa)
--- test/test_strudent.py
+++ test/test_strudent.py
@@ -3,8 +3,8 @@ from class_definitions import Student as s
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.student1 = s.Student('Doe', 'John', 'Space Exploration')
-        self.student2 = s.Student('Doe', 'John', 'Space Exploration', 3.7)
+        self.student1 = s.Student('Doe', 'John', 'Criminal Justice')
+        self.student2 = s.Student('Doe', 'John', 'Criminal Justice', 3.7)
 
     def tearDown(self):
         del self.student1
@@ -13,17 +13,17 @@ class MyTestCase(unittest.TestCase):
     def test_object_created_required_attributes(self):
         self.assertEqual(self.student1.first_name, "John")
         self.assertEqual(self.student1.last_name, "Doe")
-        self.assertEqual(self.student1.major, "Space Exploration")
+        self.assertEqual(self.student1.major, "Criminal Justice")
         self.assertEqual(self.student1.gpa, 0.0)
 
     def test_object_created_all_attributes(self):
         self.assertEqual(self.student2.first_name, "John")
         self.assertEqual(self.student2.last_name, "Doe")
-        self.assertEqual(self.student2.major, "Space Exploration")
+        self.assertEqual(self.student2.major, "Criminal Justice")
         self.assertEqual(self.student2.gpa, 3.7)
 
     def test_student_str(self):
-        self.assertEqual(str(self.student2), "Doe, John has major Space Exploration with gpa: 3.7")
+        self.assertEqual(str(self.student2), "Doe, John has major Criminal Justice with gpa: 3.7")
 
     def test_object_not_created_error_last_name(self):
         with self.assertRaises(ValueError):
@@ -33,6 +33,13 @@ class MyTestCase(unittest.TestCase):
         with self.assertRaises(ValueError):
             stu = s.Student("Chirstensen", "M0rg8n", "Cybersecurity", 3.8)
 
+    def test_object_not_created_error_major(self):
+        with self.assertRaises(ValueError):
+            stu = s.Student("Chirstensen", "M0rg8n", "World Domination", 3.8)
+
+    def test_object_not_created_error_gpa(self):
+        with self.assertRaises(ValueError):
+            stu = s.Student("Chirstensen", "M0rg8n", "Cybersecurity", 3.8)
 
 
 
