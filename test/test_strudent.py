991fdd6997c0b646e09534c5c3d5497a050fbf06
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-31 10:30:24 -0500

    Unit test for checking all attributes added passes

--- test/test_strudent.py
+++ test/test_strudent.py
@@ -3,7 +3,7 @@ from class_definitions import Student as s
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.student = s.Student('Doe', 'John', 'Space exploration')
+        self.student = s.Student('Doe', 'John', 'Space Exploration', 3.7)
 
     def tearDown(self):
         del self.student
@@ -11,7 +11,15 @@ class MyTestCase(unittest.TestCase):
     def test_object_created_required_attributes(self):
         self.assertEqual(self.student.first_name, "John")
         self.assertEqual(self.student.last_name, "Doe")
-        self.assertEqual(self.student.major, "Space exploration")
+        self.assertEqual(self.student.major, "Space Exploration")
+
+    def test_object_created_all_attributes(self):
+        self.assertEqual(self.student.first_name, "John")
+        self.assertEqual(self.student.last_name, "Doe")
+        self.assertEqual(self.student.major, "Space Exploration")
+        self.assertEqual(self.student.gpa, 3.7)
+
+
 
 
 
